691e3f69b857325462f610a0bc7c1c4807ae8cdc
YuichiOta <rb19780519@gmail.com>
2024-08-17 16:26:30 +0900

    mod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
-import { withContentlayer } from "next-contentlayer";
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { reactStrictMode: true, swcMinify: true };
 
-export default withContentlayer(nextConfig);
+module.exports = withContentlayer(nextConfig);
